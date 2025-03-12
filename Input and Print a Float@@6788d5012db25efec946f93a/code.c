1818c5d6cebad2e0f089f459e5217985bd4c40d5
VishnuSharmaaa <vishnusharma0230@gmail.com>
2025-03-12 15:36:25 +0530

    Solution to Input and Print a Float

--- Input and Print a Float@@6788d5012db25efec946f93a/code.c
+++ Input and Print a Float@@6788d5012db25efec946f93a/code.c
@@ -3,7 +3,7 @@
 int main() {
     float a;
     scanf("%f", &a);
-    printf("%.3f", a);  // Prints the number with 3 decimal places
+    printf("%.2f", a);  // Prints the number with 3 decimal places
     return 0;
 }
 
